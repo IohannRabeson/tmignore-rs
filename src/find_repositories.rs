d7bfa7246bf2407b5fffa58322ea033d3c8d26d7
irabeson <iohannrabeson@users.noreply.github.com>
2026-02-14 21:10:24 -0500

    Implement find_repositories.

---
+++ src/find_repositories.rs
@@ -0,0 +1,82 @@
+use std::{ffi::OsStr, path::{Path, PathBuf}};
+
+pub fn find_repositories(directories: &[impl AsRef<Path>]) -> Vec<PathBuf> {
+    const DOT_GIT_DIRECTORY_NAME: &str = ".git";
+
+    if directories.is_empty() {
+        return vec![]
+    }
+
+    let mut results = vec![];    
+    let walker = create_walk_builder(directories).build();
+    for entry in walker {
+        if let Ok(entry) = entry
+        && entry.path().is_dir()
+        && entry.file_name() == OsStr::new(DOT_GIT_DIRECTORY_NAME)
+        && let Some(parent) = entry.path().parent()
+        {
+            results.push(parent.to_path_buf());
+        }
+    }
+    results
+}
+
+fn create_walk_builder(directories: &[impl AsRef<Path>]) -> ignore::WalkBuilder {
+    let mut builder = ignore::WalkBuilder::new(&directories[0]);
+
+    for directory in directories.iter().skip(1) {
+        builder.add(directory);
+    }
+
+    builder.hidden(false);
+
+    builder
+}
+
+#[cfg(test)]
+mod tests {
+    use temp_dir_builder::TempDirectoryBuilder;
+
+    use crate::find_repositories::find_repositories;
+
+    #[test]
+    fn test_root_directory() {
+        let temp_dir = TempDirectoryBuilder::default()
+            .add_directory(".git")
+            .build()
+            .unwrap();
+
+        let repositories = find_repositories(&[temp_dir.path()]);
+
+        assert_eq!(repositories.len(), 1);
+        assert_eq!(repositories[0], temp_dir.path());
+    }
+
+    #[test]
+    fn test_sub_directory() {
+        let temp_dir = TempDirectoryBuilder::default()
+            .add_directory("subdirectory/.git")
+            .build()
+            .unwrap();
+
+        let repositories = find_repositories(&[temp_dir.path()]);
+
+        assert_eq!(repositories.len(), 1);
+        assert_eq!(repositories[0], temp_dir.path().join("subdirectory"));
+    }
+
+    #[test]
+    fn test_nested_repositories() {
+        let temp_dir = TempDirectoryBuilder::default()
+            .add_directory("subdirectory/.git")
+            .add_directory("subdirectory/sub_subdirectory/.git")
+            .build()
+            .unwrap();
+
+        let repositories = find_repositories(&[temp_dir.path()]);
+
+        assert_eq!(repositories.len(), 2);
+        assert!(repositories.contains(&temp_dir.path().join("subdirectory").join("sub_subdirectory")));
+        assert!(repositories.contains(&temp_dir.path().join("subdirectory")));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod config;
+mod find_repositories;
 
 use std::error::Error;
 
@@ -23,7 +24,7 @@ enum Commands {
 fn main() -> Result<(), Box<dyn Error>> {
     const CONFIG_FILE_PATH: &str = "~/.config/tmignore/config.json";
     let cli = Cli::parse();
-    let config = Config::load_or_create_file(CONFIG_FILE_PATH)?;
+    let config = Config::load_or_create_file(shellexpand::tilde(CONFIG_FILE_PATH).to_string())?;
     match cli.command {
         Commands::Run => run_command::execute(&config),
         Commands::List => list_command::execute(&config),
@@ -34,10 +35,15 @@ fn main() -> Result<(), Box<dyn Error>> {
 mod run_command {
     use std::error::Error;
 
-    use crate::config::Config;
+    use crate::{config::Config, find_repositories::find_repositories};
 
     pub fn execute(config: &Config) -> Result<(), Box<dyn Error>> {
-        println!("run");
+        let repositories = find_repositories(&config.search_directories);
+        
+        for repository in repositories {
+            println!(" - {}", repository.display());
+        }
+        
         Ok(())
     }
 }
