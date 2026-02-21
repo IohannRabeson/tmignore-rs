7b43807e44517f376b46d0d9f125461f99d9ad7b
irabeson <iohannrabeson@users.noreply.github.com>
2026-02-20 20:49:34 -0500

    Refactored Config and implement tests.

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,7 @@
 use std::{
     collections::BTreeSet,
     fs::File,
-    io::BufReader,
+    io::{BufReader, Read},
     path::{Path, PathBuf},
 };
 
@@ -19,7 +19,7 @@ pub struct Config {
 }
 
 #[derive(thiserror::Error, Debug)]
-pub enum LoadFromFileError {
+pub enum LoadError {
     #[error(transparent)]
     Io(#[from] std::io::Error),
     #[error(transparent)]
@@ -29,11 +29,28 @@ pub enum LoadFromFileError {
 }
 
 #[derive(thiserror::Error, Debug)]
-pub enum ValidationError {
+pub struct ValidationError {
+    pub fails: Vec<ValidationFail>,
+}
+
+impl std::fmt::Display for ValidationError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Validation error:\n")?;
+        for fail in &self.fails {
+            write!(f, " - {}", fail)?;
+        }
+        Ok(())
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum ValidationFail {
     #[error("File in searchPaths: {0}")]
     FileInSearchPaths(PathBuf),
     #[error("File in ignoredPaths: {0}")]
     FileInIgnoredPaths(PathBuf),
+    #[error("Not found {0}")]
+    NotFound(PathBuf),
     #[error("No search directories")]
     NoSearchDirectories,
 }
@@ -53,13 +70,13 @@ fn expand_paths(paths: &BTreeSet<PathBuf>) -> BTreeSet<PathBuf> {
 }
 
 impl Config {
-    pub fn load_or_create_file(file_path: impl AsRef<Path>) -> Result<Self, LoadFromFileError> {
+    pub fn load_or_create_file(file_path: impl AsRef<Path>) -> Result<Self, LoadError> {
         let file_path = file_path.as_ref();
 
-        let mut config = if file_path.is_file() {
+        if file_path.is_file() {
             Self::load_from_file(file_path)
         } else {
-            let default_config = Self::default();
+            let mut default_config = Self::default();
 
             std::fs::create_dir_all(file_path.parent().unwrap())?;
 
@@ -69,15 +86,22 @@ impl Config {
 
             println!("Created configuration file '{}'", file_path.display());
 
+            Self::expand(&mut default_config);
+
             Ok(default_config)
-        };
-
-        if let Ok(config) = config.as_mut() {
-            Self::expand(config);
-            Self::validate(config)?;
         }
+    }
 
-        config
+    pub fn load_from_file(file_path: impl AsRef<Path>) -> Result<Self, LoadError> {
+        Self::load(File::open(file_path)?)
+    }
+
+    pub fn load(reader: impl Read) -> Result<Self, LoadError> {
+        let reader = BufReader::new(reader);
+        let mut config = serde_json::from_reader(reader)?;
+        Self::expand(&mut config);
+        Self::validate(&config)?;
+        Ok(config)
     }
 
     fn expand(config: &mut Config) {
@@ -85,35 +109,42 @@ impl Config {
         config.ignored_directories = expand_paths(&config.ignored_directories);
     }
 
-    fn load_from_file(file_path: impl AsRef<Path>) -> Result<Self, LoadFromFileError> {
-        let file = File::open(file_path)?;
-        let reader = BufReader::new(file);
-
-        Ok(serde_json::from_reader(reader)?)
-    }
-
     fn validate(config: &Config) -> Result<(), ValidationError> {
+        let mut fails = Vec::new();
+
         for path in &config.search_directories {
             if path.is_file() {
-                return Err(ValidationError::FileInSearchPaths(path.clone()));
+                fails.push(ValidationFail::FileInSearchPaths(path.clone()));
+            } else if !path.is_dir() {
+                fails.push(ValidationFail::NotFound(path.clone()));
             }
         }
 
         for path in &config.ignored_directories {
             if path.is_file() {
-                return Err(ValidationError::FileInIgnoredPaths(path.clone()));
+                fails.push(ValidationFail::FileInIgnoredPaths(path.clone()));
             }
         }
 
         if config.search_directories.is_empty() {
-            return Err(ValidationError::NoSearchDirectories);
+            fails.push(ValidationFail::NoSearchDirectories);
         }
+
+        if fails.is_empty() {
+            Ok(())
+        } else {
+            Err(ValidationError { fails })
+        }
+    }
+
+    pub fn reload_file(&mut self, file_path: impl AsRef<Path>) -> Result<(), LoadError> {
+        self.reload(std::fs::File::open(file_path)?)?;
 
         Ok(())
     }
 
-    pub fn reload(&mut self, file_path: impl AsRef<Path>) -> Result<(), LoadFromFileError> {
-        *self = Self::load_from_file(file_path)?;
+    pub fn reload(&mut self, reader: impl Read) -> Result<(), LoadError> {
+        *self = Self::load(reader)?;
         Self::expand(self);
         Self::validate(self)?;
         Ok(())
@@ -140,4 +171,133 @@ impl Default for Config {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use temp_dir_builder::TempDirectoryBuilder;
+
+    use crate::config::{Config, LoadError};
+
+    #[test]
+    fn test_expand_default() {
+        let temp_dir = TempDirectoryBuilder::default().build().unwrap();
+        let config_file_path = temp_dir.path().join("does_not_exist.json");
+        let config = Config::load_or_create_file(&config_file_path).unwrap();
+
+        assert!(config.search_directories.len() > 0);
+        assert!(config.ignored_directories.len() > 0);
+        assert!(
+            config
+                .search_directories
+                .iter()
+                .all(|path| !path.to_str().unwrap().contains('~'))
+        );
+        assert!(
+            config
+                .ignored_directories
+                .iter()
+                .all(|path| !path.to_str().unwrap().contains('~'))
+        );
+        assert!(config_file_path.is_file());
+    }
+
+    #[test]
+    fn test_expand_loaded() {
+        let json = r#"
+{ "searchPaths": ["~"],
+  "ignoredPaths": [],
+  "whitelist": [] }
+"#;
+        let config = Config::load(json.as_bytes()).unwrap();
+        let search_directories: Vec<_> = config.search_directories.iter().collect();
+
+        assert_eq!(1, search_directories.len());
+        assert!(!search_directories[0].to_str().unwrap().contains('~'));
+    }
+
+    #[test]
+    fn test_expand_reloaded() {
+        let json = r#"
+{ "searchPaths": ["~"],
+  "ignoredPaths": [],
+  "whitelist": [] }
+"#;
+        let mut config = Config::load(json.as_bytes()).unwrap();
+        config.reload(json.as_bytes()).unwrap();
+
+        let search_directories: Vec<_> = config.search_directories.iter().collect();
+        assert_eq!(1, search_directories.len());
+        assert!(!search_directories[0].to_str().unwrap().contains('~'));
+    }
+
+    #[test]
+    fn test_missing_required_field() {
+        let json = r#"
+{
+"ignoredPaths": [],
+"whitelist": [] }
+"#;
+        let result = Config::load(json.as_bytes());
+
+        assert!(matches!(result, Err(LoadError::Json(_))));
+    }
+
+    #[test]
+    fn test_search_paths_does_not_exist() {
+        let json = r#"
+{
+"searchPaths": ["/does_not_exist"],
+"ignoredPaths": [],
+"whitelist": [] }
+"#;
+        let result = Config::load(json.as_bytes());
+
+        assert!(matches!(result, Err(LoadError::Validation(_))));
+    }
+
+    #[test]
+    fn test_no_search_paths() {
+        let json = r#"
+{
+"searchPaths": [],
+"ignoredPaths": [],
+"whitelist": [] }
+"#;
+        let result = Config::load(json.as_bytes());
+
+        assert!(matches!(result, Err(LoadError::Validation(_))));
+    }
+
+    #[test]
+    fn test_reload() {
+        let json = r#"
+{ "searchPaths": ["~"],
+  "ignoredPaths": [],
+  "whitelist": [] }
+"#;
+        let mut config = Config::load(json.as_bytes()).unwrap();
+        let json = r#"
+{ "searchPaths": ["~"],
+  "ignoredPaths": ["~"],
+  "whitelist": [] }
+"#;
+        config.reload(json.as_bytes()).unwrap();
+
+        assert!(!config.ignored_directories.is_empty());
+    }
+
+    #[test]
+    fn test_reload_error() {
+        let json = r#"
+{ "searchPaths": ["~"],
+  "ignoredPaths": [],
+  "whitelist": [] }
+"#;
+        let mut config = Config::load(json.as_bytes()).unwrap();
+        let invalid_json = r#"
+{ "searchPaths": [],
+  "ignoredPaths": [],
+  "whitelist": [] }
+"#;
+        assert!(config.reload(invalid_json.as_bytes()).is_err());
+        assert_eq!(1, config.search_directories.len());
+    }
+}
