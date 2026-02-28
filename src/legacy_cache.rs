5fcb0a8471a1fb0c96f4dbb0f8e48d96651177fa
irabeson <iohannrabeson@users.noreply.github.com>
2026-02-28 16:19:23 -0500

    Fix issue where it's not clear the dry mode is enabled when importing the legacy cache.

--- src/legacy_cache.rs
+++ src/legacy_cache.rs
@@ -5,31 +5,24 @@ use std::{
 
 use serde::Deserialize;
 
+use crate::Logger;
+
 #[derive(Deserialize)]
 pub struct LegacyCache {
     paths: Vec<PathBuf>,
 }
 
 impl LegacyCache {
-    pub fn import(cache_file_path: impl AsRef<Path>) -> Result<Vec<PathBuf>, std::io::Error> {
-        Self::load_file(cache_file_path)
-    }
-
-    fn load_file(cache_file_path: impl AsRef<Path>) -> Result<Vec<PathBuf>, std::io::Error> {
+    pub fn import(cache_file_path: impl AsRef<Path>, logger: &mut Logger) -> Result<Vec<PathBuf>, std::io::Error> {
         let cache_file_path = cache_file_path.as_ref();
         if !cache_file_path.is_file() {
             return Ok(vec![]);
         }
-        println!(
-            "Importing legacy cache file '{}'...",
-            cache_file_path.display()
-        );
+        logger.log(format!("Importing legacy cache file '{}'...", cache_file_path.display()));
         let file = std::fs::File::open(&cache_file_path)?;
         let cache: Self = serde_json::from_reader(BufReader::new(file))?;
-        println!(
-            "Imported successfully! You can now delete '{}' if you wish.",
-            cache_file_path.display()
-        );
+        logger.log(format!("Imported successfully! You can now delete '{}' if you wish.",
+            cache_file_path.display()));
         Ok(cache.paths)
     }
 }
@@ -40,7 +33,7 @@ mod tests {
 
     use temp_dir_builder::TempDirectoryBuilder;
 
-    use crate::legacy_cache::LegacyCache;
+    use crate::{Logger, legacy_cache::LegacyCache};
 
     #[test]
     fn test_import_legacy_cache() {
@@ -50,8 +43,8 @@ mod tests {
             .build()
             .unwrap();
         let cache_file_path = temp_dir.path().join("cache.json");
-
-        let cache = LegacyCache::load_file(cache_file_path).unwrap();
+        let mut logger = Logger::new(false);
+        let cache = LegacyCache::import(cache_file_path, &mut logger).unwrap();
 
         assert_eq!(2, cache.len());
         assert_eq!(PathBuf::from("a"), cache[0]);
